981a5ccead44e091e0ce9d99b7578ee7f7654f39
Kim Tran <lucian457fcde@gmail.com>
2025-02-21 12:06:39 +0700

    ad

--- src/controller/imageProcessController.js
+++ src/controller/imageProcessController.js
@@ -13,21 +13,33 @@ export async function downloadImage(imageUrl) {
 
         const filePath = `./uploads/temp_image.jpg`
         const writer = fs.createWriteStream(filePath)
-        response.data.pipe(writer)
-        writer.on('finish', async () => {
-            console.log("Image loaded to server successfully. ", filePath)
+
+        // response.data.pipe(writer)
+        // writer.on('finish', async () => {
+        //     console.log("Image loaded to server successfully. ", filePath)
+        // })
+        // return filePath
+
+        return new Promise((resolve, reject) => {
+            response.data.pipe(writer)
+            writer.on("finish", () => {
+                console.log("Image loaded to server successfully. ", filePath)
+                resolve(filePath)
+            })
+            writer.on("error", (er) => {
+                reject(er)
+            })
         })
-        return filePath
     } catch (e) {
         console.log(`Lỗi xử lý ảnh: ${e}`)
     }
 }
 
-export async function postToAWS(filePath){
+export async function postToAWS(filePath) {
     let result
-    fs.readFile(filePath, async (err, data)=>{
-        if (err){
-            console.log("Failed to read file. ",err)
+    fs.readFile(filePath, async (err, data) => {
+        if (err) {
+            console.log("Failed to read file. ", err)
         }
         result = await predict(data)
     })
